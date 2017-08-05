6eccadcb2d7edb68c43a3445edbc9d6b93215e6a
Kristiyan Goleminov <kristiyangoleminov@gmail.com>
2017-08-05 22:48:37 +0300

    Angular course complete, first commit didnt add anything

--- TheWorld/TheWorld/Controllers/Api/StopsController.cs
+++ TheWorld/TheWorld/Controllers/Api/StopsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,6 +14,7 @@ using TheWorld.ViewModels;
 namespace TheWorld.Controllers.Api
 {
     [Route("api/trips/{tripName}/stops")]
+    [Authorize]
     public class StopsController : Controller
     {
         private ILogger<StopsController> _logger;
@@ -33,7 +35,7 @@ namespace TheWorld.Controllers.Api
         {
             try
             {
-                var trip = _repository.GetTripByName(tripName);
+                var trip = _repository.GetUserTripByName(tripName, User.Identity.Name);
 
                 return Ok(Mapper.Map<IEnumerable<StopViewModel>>(trip.Stops.OrderBy(s => s.Order)));
             }
@@ -63,7 +65,7 @@ namespace TheWorld.Controllers.Api
                         newStop.Latitude = (double)result.Latitude;
                         newStop.Longitude = (double)result.Longitude;
 
-                        _repository.AddStop(tripName, newStop);
+                        _repository.AddStop(tripName, newStop, User.Identity.Name);
 
                         if (await _repository.SaveChangesAsync())
                         {
---
+++ TheWorld/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,46 @@
+﻿(function () {
+
+    "use strict";
+
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController($http) {
+
+        var vm = this;
+
+        vm.trips = [];
+
+        vm.newTrip = {};
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                angular.copy(response.data, vm.trips);
+            }, function (error) {
+                vm.errorMessage = "Failed to load data : " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+
+        vm.addTrip = function () {
+            vm.isBusy = true;
+            vm.errorMessage = "";
+
+            $http.post("/api/trips", vm.newTrip)
+                .then(function (response) {
+                    vm.trips.push(response.data);
+                    vm.newTrip = {};
+                }, function (error) {
+                    vm.errorMessage = "Failed to save new trip: " + error;
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+        }
+    }
+
+})();
